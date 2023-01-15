432b57b177de3087a629541937609c0ec8d78c2c
Richelle <richelle.ak@gmail.com>
2023-01-14 16:56:24 -0800

    BUGFIX

--- src/Currency-Exchanger.js
+++ src/Currency-Exchanger.js
@@ -1,8 +1,10 @@
 export default class CurrencyRates {
   static async getCurrency(amount){
     try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/API_KEY/latest/USD
-      ?=`)
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/d6e7ee65568b0fa5d8b71229
+      /latest/USD?=${amount}`)
+      // const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}
+      // /latest/USD?=`)
       //double check api key value
       const jsonResponse = await response.json();
       if (!response.ok){
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import CurrencyRates from "./Currency-Exchanger";
 
-async function getCurrency(amount){
-  const response = await CurrencyRates.getCurrency(amount);
+async function getCurrency(amount, currSelect){
+  const response = await CurrencyRates.getCurrency(amount, currSelect);
   if (response.result === "success"){
-    printCurrency(response, amount);
+    printCurrency(response, amount, currSelect);
+    console.log("printCurrency");
   }else {
-    printError(amount);
+    printError(amount, currSelect);
   }
 }
 
-function printCurrency(response, amount) {
+function printCurrency(response, amount, currSelect) {
 const currSelect = document.querySelector('#currency-name').value;
   if (currSelect === "euro"){
     document.querySelector('#showResponse').innerText = `$${amount} is ${response.conversion_rates.EUR * amount}.`;
@@ -22,16 +23,23 @@ const currSelect = document.querySelector('#currency-name').value;
   }else if (currSelect === "swiss-franc"){
     document.querySelector('#showResponse').innerText = `$${amount} is ${response.conversion_rates.CHF * amount}.`;
   }
+  console.log("print currency funtion");
 }
 
-function handleFormSubmission(event) {
+function printError(amount){
+  console.log("printerror");
+  document.querySelector(`#showResponse`).innerText = `There was an error accessing the currency converter for ${amount}: We were unable to find information on that location. Please try again.`;
+}
+
+function handleForm(event) {
   event.preventDefault();
   const amount = document.querySelector('#currency-amt').value;
   document.querySelector('#currency-amt').value = null;
-  getCurrency(amount);
+  const currSelect = document.querySelector('#currency-name').value;
+  getCurrency(amount, currSelect);
 }
 
 
-window.addEventListener("load", function() {
-  document.querySelector('form').addEventListener("submit", handleFormSubmission);
+window.addEventListener('load', function() {
+  document.querySelector('form').addEventListener('submit', handleForm);
 });
