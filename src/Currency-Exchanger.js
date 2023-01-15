9b820311044cabb2a71584e7c13bffdd452f0910
Richelle <richelle.ak@gmail.com>
2023-01-14 18:40:21 -0800

    push to github

--- src/Currency-Exchanger.js
+++ src/Currency-Exchanger.js
@@ -1,8 +1,9 @@
 export default class CurrencyCounter {
   static async getCurrency() {
     try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+      const response = await fetch(`https://v6.exchangerat-api.com/v6/${process.env.API_KEY}/latest/USD`);
       const jsonResponse = await response.json();
+      console.log(response);
       if (!response.ok) {
         const errorMessage = `${response.status} ${response.statusText} ${jsonResponse.message}`;
         throw new Error(errorMessage);
--- src/index.js
+++ src/index.js
@@ -6,53 +6,36 @@ async function getCurrency(amount, currSelect) {
   if (apiResponse.documentation) {
     printCurrency(apiResponse, amount, currSelect);
   } else {
-    console.log("error")
-   // printError(apiResponse, searchTerm); 
+   printError(amount, currSelect); 
   }
 }
 
-// function getCurrency(amount, currSelect){
-//   let request = new XMLHttpRequest();
-//   // const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
-//   const url = `https://v6.exchangerate-api.com/v6/d6e7ee65568b0fa5d8b71229 /latest/USD?=`;
-//   request.addEventListener("loadend", function() {
-//     const apiResponse = JSON.parse(this.responseText);
-//     if (this.status === 200){
-//      printCurrency(apiResponse, amount, currSelect)
-//     }else{
-//       document.querySelector(`#showResponse`).innerText = `Error Message: ${this.status}. Rub your magic lamp and try again, my friend.`;
-
-//     }
-// });
-// request.open("GET", url, true);
-// request.send();
-// }
 
 function printCurrency(apiResponse, amount, currSelect) {
 
   if (currSelect === "euro"){
     const eurTotal = (apiResponse.conversion_rates.EUR * amount).toFixed(2);
-    document.querySelector('#showResponse').innerText = `$${amount} USD is ${eurTotal} in ${currSelect}s.`;
+    document.querySelector('#showResponse').innerText = `$${amount} USD converts to ${eurTotal} ${currSelect}s.`;
   }else if (currSelect === "yen"){
     const jpyTotal = (apiResponse.conversion_rates.JPY * amount).toFixed(2);
-    document.querySelector('#showResponse').innerText = `$${amount} USD is ${jpyTotal} in ${currSelect}.`;
+    document.querySelector('#showResponse').innerText = `$${amount} USD converts to ${jpyTotal} ${currSelect}.`;
   }else if (currSelect === "pound"){
     const gbpTotal = (apiResponse.conversion_rates.GBP * amount).toFixed(2);
-    document.querySelector('#showResponse').innerText = `$${amount} USD is ${gbpTotal} in ${currSelect}s.`;
+    document.querySelector('#showResponse').innerText = `$${amount} USD converts to ${gbpTotal} ${currSelect}s.`;
   }else if (currSelect === "can-dollar"){
     const cadTotal = (apiResponse.conversion_rates.CAD * amount).toFixed(2);
-    document.querySelector('#showResponse').innerText = `$${amount} USD is ${cadTotal} in ${currSelect}s.`;
+    document.querySelector('#showResponse').innerText = `$${amount} USD converts to ${cadTotal} ${currSelect}s.`;
   }else if (currSelect === "swiss-franc"){
     const chfTotal = (apiResponse.conversion_rates.CHF * amount).toFixed(2);
-    document.querySelector('#showResponse').innerText = `$${amount} USD is ${chfTotal} in ${currSelect}s.`;
+    document.querySelector('#showResponse').innerText = `$${amount} USD converts to ${chfTotal} ${currSelect}s.`;
   }
   console.log("print currency funtion");
 }
 
-// function printError(amount){
-//   console.log("printerror");
-//   document.querySelector(`#showResponse`).innerText = `There was an error accessing the currency converter for ${amount}: We were unable to find information on that location. Please try again.`;
-// }
+function printError(amount, currSelect){
+  console.log("printerror");
+  document.querySelector(`#showResponse`).innerText = `There was an error accessing the currency converter for ${amount} in ${currSelect}: We were unable to find information on that currency. Please try again.`;
+}
 
 function handleForm(event) {
   event.preventDefault();
