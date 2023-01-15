126fbcd5a8ad9bcfdd5ba3ba657104b77464fd11
Richelle <richelle.ak@gmail.com>
2023-01-14 18:02:24 -0800

    update printCurrency function

--- src/Currency-Exchanger.js
+++ src/Currency-Exchanger.js
@@ -1,19 +1,36 @@
-export default class CurrencyRates {
-  static async getCurrency(amount){
-    try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/d6e7ee65568b0fa5d8b71229
-      /latest/USD?=${amount}`)
-      // const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}
-      // /latest/USD?=`)
-      //double check api key value
-      const jsonResponse = await response.json();
-      if (!response.ok){
-        const errorMessage = `${response.status} ${response.statusText} ${jsonResponse.message}`;
-        throw new Error(errorMessage);
-      }
-      return jsonResponse;
-    }catch(error){
-      return error;
-    }
-  }
-}
+// export default class CurrencyRates {
+//   static async getCurrency(amount){
+//     try {
+//       const response = await fetch(`https://v6.exchangerate-api.com/v6/d6e7ee65568b0fa5d8b71229/latest/USD
+//       ?=1`);
+//       console.log(response);
+//       // const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}
+//       // /latest/USD?=`)
+//       //double check api key value
+//       const jsonResponse = await response.json();
+//       if (!response.ok){
+//         const errorMessage = `${response.status} ${response.statusText} ${jsonResponse.message}`;
+//         throw new Error(errorMessage);
+//       }
+//       return jsonResponse;
+//     }catch(error){
+//       return error;
+//     }
+//   }
+// }
+
+// export default function getCurrency(amount, currSelect){
+//     let request = new XMLHttpRequest();
+//     const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD?=${amount}`;
+//     request.addEventListener("loadend", function() {
+//       const apiResponse = JSON.parse(this.responseText);
+//       if (this.status === 200){
+//        printCurrency(apiResponse, amount, currSelect)
+//       }else{
+//         document.querySelector(`#showResponse`).innerText = `Error Message: ${this.status}. Rub your magic lamp and try again, my friend.`;
+
+//       }
+//   });
+//   request.open("GET", url, true);
+//   request.send();
+// }
--- src/index.js
+++ src/index.js
@@ -1,35 +1,56 @@
-import CurrencyRates from "./Currency-Exchanger";
+// import getCurrency from "./Currency-Exchanger";
 
-async function getCurrency(amount, currSelect){
-  const response = await CurrencyRates.getCurrency(amount, currSelect);
-  if (response.result === "success"){
-    printCurrency(response, amount, currSelect);
-    console.log("printCurrency");
-  }else {
-    printError(amount, currSelect);
-  }
+// async function getCurrency(amount, currSelect){
+//   const response = await CurrencyRates.getCurrency(amount, currSelect);
+//   if (response.result === "success"){
+//     printCurrency(response, amount, currSelect);
+//     console.log("printCurrency");
+//   }else {
+//     printError(amount, currSelect);
+//   }
+// }
+
+function getCurrency(amount, currSelect){
+  let request = new XMLHttpRequest();
+  const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+  request.addEventListener("loadend", function() {
+    const apiResponse = JSON.parse(this.responseText);
+    if (this.status === 200){
+     printCurrency(apiResponse, amount, currSelect)
+    }else{
+      document.querySelector(`#showResponse`).innerText = `Error Message: ${this.status}. Rub your magic lamp and try again, my friend.`;
+
+    }
+});
+request.open("GET", url, true);
+request.send();
 }
 
-function printCurrency(response, amount, currSelect) {
-const currSelect = document.querySelector('#currency-name').value;
+function printCurrency(apiResponse, amount, currSelect) {
+
   if (currSelect === "euro"){
-    document.querySelector('#showResponse').innerText = `$${amount} is ${response.conversion_rates.EUR * amount}.`;
+    const eurTotal = (apiResponse.conversion_rates.EUR * amount).toFixed(2);
+    document.querySelector('#showResponse').innerText = `$${amount} USD is ${eurTotal} in ${currSelect}s.`;
   }else if (currSelect === "yen"){
-    document.querySelector('#showResponse').innerText = `$${amount} is ${response.conversion_rates.JPY * amount}.`;
+    const jpyTotal = (apiResponse.conversion_rates.JPY * amount).toFixed(2);
+    document.querySelector('#showResponse').innerText = `$${amount} USD is ${jpyTotal} in ${currSelect}.`;
   }else if (currSelect === "pound"){
-    document.querySelector('#showResponse').innerText = `$${amount} is ${response.conversion_rates.GBP * amount}.`;
+    const gbpTotal = (apiResponse.conversion_rates.GBP * amount).toFixed(2);
+    document.querySelector('#showResponse').innerText = `$${amount} USD is ${gbpTotal} in ${currSelect}s.`;
   }else if (currSelect === "can-dollar"){
-    document.querySelector('#showResponse').innerText = `$${amount} is ${response.conversion_rates.CAD * amount}.`;
+    const cadTotal = (apiResponse.conversion_rates.CAD * amount).toFixed(2);
+    document.querySelector('#showResponse').innerText = `$${amount} USD is ${cadTotal} in ${currSelect}s.`;
   }else if (currSelect === "swiss-franc"){
-    document.querySelector('#showResponse').innerText = `$${amount} is ${response.conversion_rates.CHF * amount}.`;
+    const chfTotal = (apiResponse.conversion_rates.CHF * amount).toFixed(2);
+    document.querySelector('#showResponse').innerText = `$${amount} USD is ${chfTotal} in ${currSelect}s.`;
   }
   console.log("print currency funtion");
 }
 
-function printError(amount){
-  console.log("printerror");
-  document.querySelector(`#showResponse`).innerText = `There was an error accessing the currency converter for ${amount}: We were unable to find information on that location. Please try again.`;
-}
+// function printError(amount){
+//   console.log("printerror");
+//   document.querySelector(`#showResponse`).innerText = `There was an error accessing the currency converter for ${amount}: We were unable to find information on that location. Please try again.`;
+// }
 
 function handleForm(event) {
   event.preventDefault();
